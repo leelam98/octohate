205e5ff347c289966904d6edd602266b09f4bbf4
Liam Hale <leelam.hale@gmail.com>
2019-07-25 17:49:38 +0100

    gradle

--- ryn/src/test/java/omg/OneAwayTest.java
+++ ryn/src/test/java/omg/OneAwayTest.java
@@ -1,11 +1,8 @@
 package omg;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import jdk.jfr.Timestamp;
 
 /**
  * Unit test for simple App.

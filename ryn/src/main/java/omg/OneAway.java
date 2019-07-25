f5d7effb01d22e49ff1ecb7f954cd65844c5ec8e
Liam Hale <leelam.hale@gmail.com>
2019-07-25 16:39:37 +0100

    yep

--- ryn/src/main/java/omg/OneAway.java
+++ ryn/src/main/java/omg/OneAway.java
@@ -4,10 +4,26 @@ package omg;
  * Hello world!
  *
  */
-public class App 
+public class OneAway
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+    public static boolean checkForOneEdit(String string1, String string2) {
+        int matches = 0;
+        for (int i = 0; i < string1.length(); i++) {
+            String letter = Character.toString(string1.charAt(i));
+            if(string2.contains(letter)){
+                matches++;
+            }
+        }
+        if (matches == (string1.length() - 1)){
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
--- ryn/src/test/java/omg/OneAwayTest.java
+++ ryn/src/test/java/omg/OneAwayTest.java
@@ -1,20 +1,44 @@
 package omg;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import jdk.jfr.Timestamp;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class OneAwayTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrueNeedsInsertion()
     {
-        assertTrue( true );
+        String inputString1 = "pale";
+        String inputString2 = "ple";
+        Boolean actual = OneAway.checkForOneEdit(inputString1, inputString2);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void shouldAnswerWithTrueNeedsRemoval()
+    {
+        String inputString1 = "pales";
+        String inputString2 = "pale";
+        Boolean actual = OneAway.checkForOneEdit(inputString1, inputString2);
+        assertEquals(true, actual);
+    }
+
+    @Test 
+    public void shouldAnswerWithTrueNeedsReplacement()
+    {
+        String inputString1 = "pale";
+        String inputString2 = "bale";
+        Boolean actual = OneAway.checkForOneEdit(inputString1, inputString2);
+        assertEquals(true, actual);
     }
 }
